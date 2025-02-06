803990d8da14e898ceb6ea642f51a10c74fb7a8a
Murali Vennapusa <146339887+muralivennapusa@users.noreply.github.com>
2025-02-06 18:38:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+file=open("aimlapi_key.txt","r")
+key=file.read()
+
+base_url = "https://api.aimlapi.com/v1"
+api_key = key
+system_prompt = "You are a cyber case classifier consultant. Be descriptive and helpful."
+user_prompt = "Tell me what are the penalities and jail time for an offence of possession or use of tools or software intended for cybercrime and public interest litigation."
+
+api = OpenAI(api_key=api_key, base_url=base_url)
+
+completion = api.chat.completions.create(
+    model="gpt-4o-mini-2024-07-18",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ],
+    temperature=0.7,
+    max_tokens=25,
+)
+
+response = completion.choices[0].message.content
+
+print("User:", user_prompt)
+print("AI:", response)
+
